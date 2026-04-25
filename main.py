41fb840537475b3fe14e70213f4158f5f8af23f7
Plinio <plinioigt@gmail.com>
2026-04-25 18:18:42 -0300

    feat: added confusion_matrix and measurement of model succes by different metrics (accuracy, sensibility, specificity, precision, f1-score)

--- main.py
+++ main.py
@@ -46,4 +46,22 @@ acurácia: nº de acertos / nº de casos de teste.
 
 tester = MonteCarloTester(M_test, W)
 
-tester.run_test()
+confusion_matrix = tester.run_test()
+
+accuracy, sensibility, specificity, precision, f1_score = tester.calcutate_validation_metrics(confusion_matrix)
+
+'''
+Na primeira vez que rodei, retornou:
+0.7964285714285714 0.975 0.35 0.7894736842105263 0.43624161073825507
+
+isso significa:
+79.64% de acurácia, que a principio parece ser um valor interessante de acerto.
+97.5% de sensiblidade,o que indica alto acerto de positivos (Verdadeiros Positivos / True Positives)
+Mas a especificidade mostra o erro crasso do modelo (perceptron simples): 35% de acerto, ou seja, para valores negativos, houve altissima falha,
+mostrando que o modelo está enviesado para valores positivos e só acerta eles devido maior densidade de amostras que retornam positivo.
+Precisão de 78.94% demonstra que a maior parte dos positivos preditos realmente são positivos
+
+Mas o f1-score mostra que a média harmônica entre a precisão e a sensibilidade mostra que o modelo foi teve baixa performance (43.62%),
+mostrando que o modelo é péssimo em situaçaõ onde o resultante final é negativo.
+'''
+print(accuracy,sensibility,specificity,precision,f1_score)
--- monte_carlo_tester.py
+++ monte_carlo_tester.py
@@ -1,11 +1,9 @@
 import numpy as np
-from data_monte_carlifier_splitter import DataMonteCarlifier
 
 class MonteCarloTester:
     def __init__(self, M_test, W):
         self.M_test = M_test
         self.W = W
-        self.hits = 0 # accuracy
 
     def _bipolar_step_activation_function(self, x):
         if x >= 0:
@@ -15,14 +13,46 @@ class MonteCarloTester:
 
     def run_test(self):
         X_test = self.M_test[:, :3]
+        confusion_matrix = [[0,0],[0,0]]
 
         for i in range(X_test.shape[0]):
             u_k = np.dot(X_test[i], self.W)
             y_k = self._bipolar_step_activation_function(u_k)
             d_k = self.M_test[i, -1]
-            if d_k == y_k:
-                self.hits += 1
 
-        accuracy = self.hits / X_test.shape[0]
-        print(accuracy)
+            if d_k == y_k and (d_k == +1 and y_k == +1):
+                confusion_matrix[0][0]+=1 #VP
 
+            if d_k == y_k and (d_k == -1 and y_k == -1):
+                confusion_matrix[1][1]+=1 #VN
+
+            if d_k != y_k and (d_k == -1 and y_k == +1):
+                confusion_matrix[0][1]+=1 #FP
+
+            if d_k != y_k and (d_k == +1 and y_k == -1):
+                confusion_matrix[1][0]+=1 #FN
+
+        return confusion_matrix
+
+    '''
+    Calcula as métricas de validação a partir da matriz de confusão retornada de run_test().
+    Acurácia: quantas vezes o modelo acertou, dividido pelo total de amostras disponíveis.
+    Sensibilidade: Mede a razão entre os valores positivos (+1) corretamente identificados dividido pelo total de valores identificados corretamente como positivo e incorretamente como negativo (que na real era positivo).
+    Especificidade: Mesma lógica da sensibilidade, mas para os negativos. Mede os corretamente identificados como negativo dividio pelo total de valores identificados corretamente como negativo e incorretamente como positivo (que na real era negativo).
+    Precisão: Proporção de valores identificados como positivo pelo total de positivos (verdadeiros e falsos)
+    F1-Score: Média harmônica entre precisão e sensibilidade, equilibrando as duas métricas. 
+    '''
+    def calcutate_validation_metrics(self, confusion_matrix):
+        VP = confusion_matrix[0][0]
+        VN = confusion_matrix[1][1]
+        FP = confusion_matrix[0][1]
+        FN = confusion_matrix[1][0]
+
+        accuracy = (VP + VN)/(VP+VN+FP+FN)
+        sensibility = VP/(VP+FN)
+        specificity = VN/(VN+FP)
+        precision = VP/(VP+FP)
+
+        f1_score = (precision * sensibility) / (precision + sensibility)
+
+        return accuracy, sensibility, specificity, precision, f1_score
